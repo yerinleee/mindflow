2ddb4ae53374570bd81bc1c72dc405b33b684953
DESKTOP-7SQJT9E\c573 <yerinlee.yr@gmail.com>
2026-01-20 22:35:49 +0900

    백엔드 생성

---
+++ mindflow-backend/src/main/java/com/mindflow/mindflowbackend/MindflowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mindflow.mindflowbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindflowBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MindflowBackendApplication.class, args);
+    }
+
+}
